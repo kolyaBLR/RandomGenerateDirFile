a96279b87fff174edfcf34972b2ad9363fd6651d
Nikolai Bobrov <bk97w@bk.ru>
2017-08-12 18:27:37 +0300

    fix output file

---
+++ README.md
@@ -0,0 +1,6 @@
+#path
+#depth
+#max size file in byte
+#max count iteration (directory and file)
+
+#max size file name 8 symbols
--- main.rb
+++ main.rb
@@ -1,18 +1,11 @@
-#путь
-#глубина
-#максимальный размер файлов
-#максимальное кол-во итераций (файлов и папок)
-
-
-#максимальная длинна имени 8 символов
-
 def generate_text(size = 8)
   syms = ['a'..'z','A'..'Z'].map{ |range| range.to_a }.flatten
   (0...size.to_i).map{ syms[ rand(syms.size) ] }.join
 end
 
-def filling_file(path, size)
-  File.open("#{path}", "w"){|file| file.write generate_text rand size}
+def generate_file(path, max_size)
+  file_name = "#{generate_text}.#{generate_text 3}"
+  File.open("#{path}/#{file_name}", "w"){|file| file.write generate_text rand max_size}
 end
 
 def create_dir_file(path, depth, max_size, max_iteration, index = 0)
@@ -21,13 +14,20 @@ def create_dir_file(path, depth, max_size, max_iteration, index = 0)
     Dir.mkdir("#{path}/#{dir_name}")
     i = 0
     while (max_iteration / depth - 1).to_i > i
-      file_name = "#{generate_text}.#{generate_text(3)}"
-      File.new("#{path}/#{dir_name}/#{file_name}", "w")
-      filling_file("#{path}/#{dir_name}/#{file_name}", max_size)
+      generate_file("#{path}/#{dir_name}", max_size)
       i += 1
     end
     create_dir_file("#{path}/#{dir_name}", depth, max_size, max_iteration, index + 1)
   end
 end
 
-create_dir_file(ARGV[0], ARGV[1].to_i, ARGV[2].to_i, ARGV[3].to_i)
+def start
+  create_dir_file(ARGV[0], ARGV[1].to_i, ARGV[2].to_i, ARGV[3].to_i)
+  i = (ARGV[3].to_i / ARGV[1].to_i).to_i * ARGV[1].to_i
+  while ARGV[3].to_i != i
+    generate_file("#{ARGV[0]}", ARGV[2].to_i)
+    i += 1
+  end
+end
+
+start
